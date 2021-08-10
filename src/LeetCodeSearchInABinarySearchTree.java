4b569620457c71df7075172a63c8c969855cdf77
Zakoemon <zoemon90@gmail.com>
2021-08-10 17:41:58 +0900

    20210810

--- src/LeetCodeSearchInABinarySearchTree.java
+++ src/LeetCodeSearchInABinarySearchTree.java
@@ -1,12 +1,14 @@
-
+//https://leetcode.com/problems/search-in-a-binary-search-tree/solution/
 public class LeetCodeSearchInABinarySearchTree {
     public static void main(String args[]) {
-        TreeNode root = new TreeNode(5);
+        TreeNode root = new TreeNode(6);
         root.left = new TreeNode(4);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(5);
         root.right = new TreeNode(9);
-        root.right.left = new TreeNode(2);
-        root.right.right = new TreeNode(5);
-        TreeNode found = searchBST(root, 8);
+        root.right.left = new TreeNode(8);
+        root.right.right = new TreeNode(10);
+        TreeNode found = better(root, 7);
         System.out.println(found == null ? null : found.val);
     }
 
@@ -27,6 +29,13 @@ public class LeetCodeSearchInABinarySearchTree {
             return null;
         }
         return match == null ? null : match;
+    }
+
+    public static TreeNode better(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        return (val > root.val) ? searchBST(root.right, val) : searchBST(root.left, val);
     }
 
     public static class TreeNode {
