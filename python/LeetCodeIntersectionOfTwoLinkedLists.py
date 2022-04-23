c162584ccaabd034790add30a0b78c7189545f1b
zakoemon <zoemon90@gmail.com>
2022-04-24 01:33:30 +0900

    20220424

---
+++ python/LeetCodeIntersectionOfTwoLinkedLists.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode):
+        
+        cur1 = headA
+        cur2 = headB
+        while cur1 is not cur2:
+            cur1 = headB if cur2 is None else cur1.next
+            cur2 = headB if cur1 is None else cur2.next
+        return cur1
